dec20a7ea16095efce520ef01855b47f08df8f4a
William McFall <wmcfall2@gmail.com>
2019-04-19 17:18:14 -0400

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(255),
+    devoured BIT.
+    PRIMARY KEY(id)
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheeseburger", 0);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Blue Cheese Cheeseburger", 0);
+
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whooper With Cheese", 1);
